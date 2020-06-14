3b6cba1f986522db66dc5275e26e2b2eb19dbbc7
syedameerhamza <65029216+syedameerhamza@users.noreply.github.com>
2020-06-14 06:11:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+					//Assignment -------------------------------------------1//
+var a;
+a="Error! Please enter a valid Password ";
+//alert(a);
+
+var b="Welcom to Js land...";
+var c="Happy Coding";
+//alert(b+"\n"+c);
+
+//alert(b);
+//alert(c+"\n"+"prevent this page from creating extra dialoge");
+var d="Hello ! This is console";
+//console.log(d);
+
+							
+							//Assignmet------------------------------------ 2//
+var username;
+var myname="Syed Ameer Hamza";
+//alert(myname);
+
+var msg="Hello world!";
+//alert(msg); 
+
+var age="22";
+var Fname="Syed Aqeel";
+
+//alert(myname);
+//alert(age);
+//alert(Fname);
+var e="pizza"
+//alert(e+"\n"+e.slice(0,4)+"\n"+e.slice(0,3)+"\n"+e.slice(0,2)+"\n"+e.slice(0,1));
+var email="my email is syedameerhamza65@gmail.com";
+//alert(email);
+
+var book=" i am tryin to learn javascript";
+//alert(book);
+
+					//Assignment--------------------------------------------3
+
+var aa="yeh i can write html content through javascript";
+//document.write(aa);
+
+var ab="***************$^>o<^$********************";
+//alert(ab);
+						
+						//Assignment----------------------------------------4
+var age=22;
+//alert("I am"+" "+age+" "+"year Old");
+
+//var N=prompt ('Enter your Visit Number');
+//alert("you Visit"+" "+N+" "+"Time");
+
+var birth="1998";
+//document.write("My Birth year is"+birth+" the declartion variable is NUMber");
+
+//var namev=prompt ("welcome to our Store",'Enter your Name');
+ //var p=prompt('Enter Product');
+ //var q=prompt ('Enter Quanatity of Things');
+ // document.write("Thanks for Shopping"+"\n"+namev+" "+"Purchase"+" "+q+" "+p+" "+" from XyzStore");
+
+						
+						//Assignment--------------------------------5
+
+
+//var num1=prompt('Enter Number');
+//var op=prompt("Choose the Operation +,-,/,* ",'Enter OPeration');
+//var num2=prompt('Enter Number');
+
+
+//if(op=="+"){
+//var add=Number(num1)+Number(num2);	
+	//docuwrite("ment.Addition"+" "+num1+op+num2+"="+add);
+//}
+//else if(op=="-"){
+	//var sub=num1-num2;
+	//document.write("Subtraction"+" "+num1+op+num2+"="+sub);
+//}
+//else if(op=="/"){
+	//var div=num1/num2;
+//	document.write("Division"+" "+num1+op+num2+"="+div);
+//}
+//else if(op=="*"){
+	//var mul=num1*num2;
+	//document.write("Multiplication"+" "+num1+op+num2+"="+mul);
+//}
+
+
+//var iv=prompt('Enter Number');
+//document.write("intail value="+iv);
+//var icv=++iv;
+//var vv=prompt('Enter Number');
+
+//document.write("<br>"+" incremnt value="+icv);
+//document.write("<br>"+"second value="+vv);
+//var sum=Number(icv)+Number(vv);	
+//document.write("<br>"+"sum="+sum);
+//var min=--sum;
+//document.write("<br>"+"after decrement="+min);
+//var md=min%3;
+//document.write("<br>"+"reminder after divide by 3="+md);	
+
+
+//var Quanatity=prompt("Enter the tickets Quanatity",'How Many Tickets you Want');
+//var cost=Quanatity*600;
+//document.write("Cost of Ticket is 600PRK"+"<br>"+"You Book="+Quanatity+" "+"Ticket"+"<br>"+"your cost="+cost);
+
+
+//var u=prompt("Enter the Number to find the Table");
+//document.write("You Enter "+u)
+//for(t=0; t<=10; t++){
+	//document.write("<br>"+u+"*"+" "+t+" "+"="+u+" "*t);	
+//}
+
+
+//var celsius=prompt('Enter Temparature in Celsius');
+//document.write("Celsius ="+celsius);
+//var Far=celsius*9/5+Number(32);
+//document.write("<br>"+"Faranhite ="+Far);
+
+//var item1q=prompt("Enter the item 1 quantity");
+//document.write("item 1 cost = 160");
+//document.write("<br>"+"you enter the item 1 Quantity="+item1q);
+//var item2q=prompt("Enter the item 2 quantity");
+//document.write("<br>"+"item 2 cost = 100");
+//document.write("<br>"+"you enter the item 1 Quantity="+item2q);
+//document.write("<br>"+"shipping Charges = 100");
+//var total=Number(item1q*160)+Number(item2q*100)+Number(100);
+//document.write("<br>"+"Total = "+total);
+
+
+//var obtain=prompt("Enter Obtain Mark");
+//var totalmark=980;
+//document.write("Obtain Mark ="+obtain);
+//document.write("<br>"+"Total Mark = 980");
+//var percentage=Number(obtain*100/980);
+//document.write("<br>"+"Percentage ="+percentage);
+
+
+//var pkus=prompt("Enter PKR to USD");
+//var dollor=pkus*104;
+//document.write(pkus+" "+"USD ="+dollor+" "+"Rupess");
